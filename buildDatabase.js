b814b991ab98f3029a2045b350448a54b8da489a
Ali Abdoli <xXAligatorXx@outlook.com>
2018-12-01 06:40:00 -0500

    fixed syntax

--- buildDatabase.js
+++ buildDatabase.js
@@ -7,63 +7,63 @@ var db = new sqlite3.Database('db.sqlite');
 const startIndex = 2079;
 
 function insertImage(body, callback) {
-    db.serialize(() => {
-        let sql = db.prepare("INSERT INTO Comics (month, link, year, transcript, alt, img, title, day, hash) VALUES ($month, $link, $year, $transcript, $alt, $img, $title, $day, $hash)");
+  db.serialize(() => {
+    let sql = db.prepare("INSERT INTO Comics (month, link, year, transcript, alt, img, title, day, hash) VALUES ($month, $link, $year, $transcript, $alt, $img, $title, $day, $hash)");
 
-        sql.run({
-            $month: parseInt(body.month, 10),
-            $link: body.link,
-            $year: parseInt(body.year, 10),
-            $transcript: body.transcript,
-            $alt: body.alt,
-            $img: body.img,
-            $title: body.title,
-            $day: parseInt(body.day, 10),
-            $hash: body.hash
-        });
-        sql.finalize();
-        callback();
-
+    sql.run({
+      $month: parseInt(body.month, 10),
+      $link: body.link,
+      $year: parseInt(body.year, 10),
+      $transcript: body.transcript,
+      $alt: body.alt,
+      $img: body.img,
+      $title: body.title,
+      $day: parseInt(body.day, 10),
+      $hash: body.hash
     });
+    sql.finalize();
+    callback();
+
+  });
 
 }
 
 function downloadImage(uri, callback) {
-    request.head(uri, function (err, res, body) {
-        
-        request(uri).pipe(fs.createWriteStream("./images/" + uri.substring(uri.lastIndexOf("/") + 1))).on('close', () => {
-            callback("./images/" + uri.substring(uri.lastIndexOf("/")));
-        });
+  request.head(uri, function (err, res, body) {
+
+    request(uri).pipe(fs.createWriteStream("./images/" + uri.substring(uri.lastIndexOf("/") + 1))).on('close', () => {
+      callback("./images/" + uri.substring(uri.lastIndexOf("/")));
     });
+  });
 };
 
 function getDHash(uri, callback) {
-    downloadImage(uri, (path) => {
-        dhash(path, function (err, hash) {
-            if (err) {
-                throw err; // fuck
-            }
-            callback(hash);
-        });
+  downloadImage(uri, (path) => {
+    dhash(path, function (err, hash) {
+      if (err) {
+        throw err; // fuck
+      }
+      callback(hash);
     });
+  });
 }
 
 function loadImages(index) {
-    if (index == 0) {
-        db.close();
-        return;
-    }
-    
-    request(`https://xkcd.com/${index}/info.0.json`, function (error, response, body) {
-        let json = JSON.parse(body);
-        console.log(index);
-        getDHash(json.img, (hash) => {
-            json.hash = hash.toString('base64');
-            insertImage(json, () => {
-                loadImages(index - 1);
-            });
-        });
+  if (index == 0) {
+    db.close();
+    return;
+  }
+
+  request(`https://xkcd.com/${index}/info.0.json`, function (error, response, body) {
+    let json = JSON.parse(body);
+    console.log(index);
+    getDHash(json.img, (hash) => {
+      json.hash = hash.toString('base64');
+      insertImage(json, () => {
+        loadImages(index - 1);
+      });
     });
+  });
 }
 
 loadImages(startIndex);
