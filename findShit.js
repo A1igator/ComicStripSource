4d85bc9a7de9b69f34ab490304b992bd5c7c1fbd
Owen Anderson <owenthomasanderson@gmail.com>
2018-12-01 12:40:03 -0500

    fixed the shit

--- findShit.js
+++ findShit.js
@@ -4,27 +4,23 @@ var db = new sqlite3.Database('db.sqlite');
 
 function findTheShit(path, callback) {
     // open a file called "lenna.png"
-    Jimp.read(path, (err, img) => {
-        if (err) throw err;
-        img.write('./imgtmp.png');
-        getDHash('./imgtmp.png', (imgHash, err) => {
-            if (imgHash == null) {
-                callback(new Error("bad img format (or missing)", null, null));
+    getDHash(path, (imgHash, err) => {
+        if (imgHash == null) {
+            callback(new Error("bad img format (or missing)", null, null));
+            return;
+        }
+        getAllTheShitFromTheDatabase((err, rows) => {
+            if (err != null) {
+                callback(err, null, null);
                 return;
             }
-            getAllTheShitFromTheDatabase((err, rows) => {
-                if (err != null) {
-                    callback(err, null, null);
-                    return;
-                }
 
-                getMinDiffForTheShit(rows, imgHash, (diff, id) => {
-                    db.serialize(() => {
-                        let sql = db.prepare("SELECT * FROM Comics WHERE _ROWID_ = $rowid");
-                        sql.get({ $rowid: id }, (err, row) => {
-                            sql.finalize();
-                            callback(err, diff, row);
-                        });
+            getMinDiffForTheShit(rows, imgHash, (diff, id) => {
+                db.serialize(() => {
+                    let sql = db.prepare("SELECT * FROM Comics WHERE num = $rowid");
+                    sql.get({ $rowid: id }, (err, row) => {
+                        sql.finalize();
+                        callback(err, diff, row);
                     });
                 });
             });
@@ -51,7 +47,7 @@ function getMinDiffForTheShit(rows, hash, callback, curMin = Infinity, curMinID 
     let diff = getTheDiffForTheShit(new Buffer(rows[0].hash, "base64"), hash);
 
     if (diff < curMin) {
-        getMinDiffForTheShit(rows.slice(1), hash, callback, diff, rows[0].rowid);
+        getMinDiffForTheShit(rows.slice(1), hash, callback, diff, rows[0].num);
     } else {
         getMinDiffForTheShit(rows.slice(1), hash, callback, curMin, curMinID);
     }
@@ -68,17 +64,17 @@ function getTheDiffForTheShit(hash1, hash2) {
 function getAllTheShitFromTheDatabase(callback) {
     db.serialize(() => {
 
-        db.all("SELECT _ROWID_, hash FROM Comics;", {}, (err, shit) => {
+        db.all("SELECT num, hash FROM Comics;", {}, (err, shit) => {
             callback(err, shit);
         });
 
     });
 }
 
-/* findTheShit("./shawn.jpg", (err, diff, id) => {
+findTheShit("./shawn.jpg", (err, diff, id) => {
     console.log(err);
     console.log(diff);
     console.log(id);
-}); */
+});
 
 module.exports.findTheShit = findTheShit;
